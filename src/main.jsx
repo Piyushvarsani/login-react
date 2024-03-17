e43a5beaca74ded9e782d576643f448cd2d4c06e
Piyush varsani <70423345+Piyushvarsani@users.noreply.github.com>
2024-03-17 21:09:36 +0530

    update again

--- src/main.jsx
+++ src/main.jsx
@@ -6,24 +6,30 @@ import Login from './login.jsx'
 import Home from './Home.jsx'
 import Error from './Error.jsx'
 
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+import { Navigate, Route, Router, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import Layout from './Layout.jsx'
 
 const root = createBrowserRouter(
   createRoutesFromElements(
     <>
+      {/*<Route path="" element={<Login />} >
+        <Route path='home' element={<Home />} />
+        <Route path='/' index element={<Login />} />
+        <Route path='signup' element={<SignUp />} />
+        <Route path='*' element={<Error />} />
+      </Route>*/}
+
       <Route path='' element={<Layout />} >
         <Route index element={<Login />} />
-        <Route path='/' element={<Home />} />
+        <Route path='home' element={<Home />} />
         <Route path='login' element={<Login />} />
         <Route path='signup' element={<SignUp />} />
         <Route path='*' element={<Error />} />
       </Route>
     </>
   ))
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={root} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
